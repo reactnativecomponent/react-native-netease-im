c7c0960f4e646f5c7d8faee340536d505b6c033f
xinhui.peng <jlchome@sina.cn>
2017-07-14 23:48:45 +0800

    fix:消息过滤

--- android/src/main/java/com/netease/im/session/SessionService.java
+++ android/src/main/java/com/netease/im/session/SessionService.java
@@ -139,7 +139,7 @@ public class SessionService {
             IMMessage m = messages.get(0);
             if (!this.mute && m.getDirect() == MsgDirectionEnum.In) {
                 if (showMsg(m)) {
-                    if (m.getAttachment() != null && ((CustomAttachment) m.getAttachment()).getType() == CustomAttachmentType.RedPacket) {
+                    if (m.getAttachment() != null && (m.getAttachment() instanceof RedPacketAttachement)) {
                         AudioPlayService.getInstance().playAudio(handler, ReactCache.getReactContext(), AudioManager.STREAM_RING, "raw", "rp");
                     } else {
                         AudioPlayService.getInstance().playAudio(handler, ReactCache.getReactContext(), AudioManager.STREAM_RING, "raw", "msg");
@@ -154,7 +154,7 @@ public class SessionService {
 
     boolean showMsg(IMMessage m) {
         return !(m.getMsgType() == MsgTypeEnum.notification || m.getMsgType() == MsgTypeEnum.tip
-                || (m.getAttachment() != null && ((CustomAttachment) m.getAttachment()).getType() == CustomAttachmentType.RedPacketOpen));
+                || (m.getAttachment() != null && (m.getAttachment() instanceof RedPacketOpenAttachement);
     }
 
     public boolean isMyMessage(IMMessage message) {
