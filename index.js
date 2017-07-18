652b78510fff98ab5b69b04877294dba8363c4fb
huangjun <hj644473762@gmail.com>
2017-07-18 17:36:03 +0800

    添加权限模块

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import NIM from './NIM';
 import AppCacheUtil from './AppCacheUtil';
-
+import Permissions from './permissions';
 export {
     NIM,
     AppCacheUtil,
+    Permissions
 };
 
 export default NIM;
