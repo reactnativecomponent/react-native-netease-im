7d2f6b8cefd6ff3a5054c5d1f285339cf6eccf47
guangyao <dowin@DowindeMac-mini-2.local>
2017-07-19 18:00:56 +0800

    fix 空数据的问题

--- ios/RNNeteaseIm/RNNeteaseIm/NIMModel.m
+++ ios/RNNeteaseIm/RNNeteaseIm/NIMModel.m
@@ -26,32 +26,34 @@
 //}
 
 - (void)setRecentDict:(NSDictionary *)recentDict{
-    if (_recentDict != recentDict) {
+    if ((_recentDict != recentDict)&&(recentDict.count)) {
         _recentDict = recentDict;
         self.myBlock(1, recentDict);
     }
 }
 
 -(void)setNetStatus:(NSString *)NetStatus{
-    if (_NetStatus != NetStatus) {
+    if ((_NetStatus != NetStatus)&&(NetStatus.length)) {
         _NetStatus = NetStatus;
         self.myBlock(0, NetStatus);
     }
 }
 -(void)setNIMKick:(NSString *)NIMKick{
-    if (_NIMKick != NIMKick) {
+    if ((_NIMKick != NIMKick)&&(NIMKick.length)) {
         _NIMKick = NIMKick;
         self.myBlock(2, NIMKick);
     }
 }
 -(void)setContactList:(NSMutableDictionary *)contactList{
-    if (_contactList != contactList) {
+    if ((_contactList != contactList)&&(contactList.count)) {
         _contactList = contactList;
         self.myBlock(3, contactList);
     }
 }
 -(void)setNotiArr:(NSMutableArray *)notiArr{
-        self.myBlock(5, notiArr);
+    if (notiArr.count) {
+       self.myBlock(5, notiArr);
+    }
 }
 -(void)setTeamArr:(NSMutableArray *)teamArr{
     if (teamArr.count) {
@@ -64,42 +66,58 @@
 }
 //
 -(void)setResorcesArr:(NSMutableArray *)ResorcesArr{
-    self.myBlock(7, ResorcesArr);
+    if (ResorcesArr.count) {
+        self.myBlock(7, ResorcesArr);
+    }
 }
 //开始发送
 -(void)setStartSend:(NSDictionary *)startSend{
-    self.myBlock(8, startSend);
+    if (startSend.count) {
+        self.myBlock(8, startSend);
+    }
 }
 //结束发送
 -(void)setEndSend:(NSDictionary *)endSend{
-    self.myBlock(9, endSend);
+    if (endSend.count) {
+        self.myBlock(9, endSend);
+    }
 }
 //发送进度（图片等附件）
 -(void)setProcessSend:(NSDictionary *)processSend{
-    self.myBlock(10, processSend);
+    if (processSend.count) {
+        self.myBlock(10, processSend);
+    }
 }
 //已读通知
 -(void)setReceipt:(NSString *)receipt{
-    self.myBlock(11, receipt);
+    if (receipt.length) {
+        self.myBlock(11, receipt);
+    }
 }
 //发送消息
 -(void)setSendState:(NSMutableArray *)sendState{
-    if (_sendState != sendState) {
+    if ((_sendState != sendState)&&(sendState.count)) {
         self.myBlock(12, sendState);
     }
 }
 //黑名单列表
 -(void)setBankList:(NSMutableArray *)bankList{
-    self.myBlock(13, bankList);
+    if (bankList.count) {
+        self.myBlock(13, bankList);
+    }
 }
+
 //录音进度
 -(void)setAudioDic:(NSDictionary *)audioDic{
-   
-    self.myBlock(14, audioDic);
+    if (audioDic.count) {
+       self.myBlock(14, audioDic);
+    }
 }
 
 - (void)setDeleteMessDict:(NSDictionary *)deleteMessDict{
-    self.myBlock(15, deleteMessDict);
+    if (deleteMessDict.count) {
+        self.myBlock(15, deleteMessDict);
+    }
 }
 
 @end
