14ceb19c257aec126f860f5f4b27cad1eb2aa5c1
xinhui.peng <jlchome@sina.cn>
2017-07-20 16:47:16 +0800

    添加updateMyUserInfo更新当前用户资料

--- android/src/main/java/com/netease/im/RNNeteaseImModule.java
+++ android/src/main/java/com/netease/im/RNNeteaseImModule.java
@@ -1319,28 +1319,19 @@ public class RNNeteaseImModule extends ReactContextBaseJavaModule implements Lif
     }
 
     /**
-     * 保存用户资料
+     * 更新用户资料
      *
+     * @param name
      * @param promise
      */
-//    @ReactMethod
-    public void updateMyUserInfo(final Promise promise) {//TODO;
-
-//        final Map<UserInfoFieldEnum, Object> fields = new HashMap<>(1);
-//        UserUpdateHelper.update(fields, new RequestCallbackWrapper<Void>() {
-//            @Override
-//            public void onResult(int code, Void result, Throwable exception) {
-//
-//                if (code == ResponseCode.RES_SUCCESS) {
-//                    LogUtil.i(TAG, "update userInfo success, update fields count=" + fields.size());
-//                } else {
-//                    if (exception != null) {
-//                        Toast.makeText(IMApplication.getContext(), R.string.user_info_update_failed, Toast.LENGTH_SHORT).show();
-//                        LogUtil.i(TAG, "update userInfo failed, exception=" + exception.getMessage());
-//                    }
-//                }
-//            }
-//        });
+    @ReactMethod
+    public void updateMyUserInfo(String name, final Promise promise) {
+        String contactId = LoginService.getInstance().getAccount();
+        NimUserInfoCache.getInstance().getUserInfoFromRemote(contactId, new RequestCallbackWrapper<NimUserInfo>() {
+            @Override
+            public void onResult(int i, NimUserInfo userInfo, Throwable throwable) {
+            }
+        });
     }
 
     /**
