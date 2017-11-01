256d9673613a7318457bcbfb047f22cecae72675
huangjun <hj644473762@gmail.com>
2017-11-01 09:20:31 +0800

    添加说明，修正语法错误

--- Utils.js
+++ Utils.js
@@ -3,79 +3,87 @@ import { NativeModules,Platform, NetInfo } from 'react-native';
 
 const { RNNeteaseIm,PinYin } = NativeModules;
 
-class Utils{
+class Utils {
     /**
      * 清除数据缓存
      */
-    cleanCache(){
+    cleanCache() {
         return RNNeteaseIm.cleanCache();
     }
 
     /**
      * 获取缓存大小
      */
-    getCacheSize(){
+    getCacheSize() {
         return RNNeteaseIm.getCacheSize();
     }
 
     /**
      * 获取网络状态
      */
-    networkIsConnected(){//0：无网络，1：有网络
-        if(Platform.OS === 'ios'){
-           if(RNNeteaseIm.getNetWorkStatus() === '1'){
-               return true;
-           }
-           return false;
-        }else{
+    networkIsConnected() {//0：无网络，1：有网络
+        if (Platform.OS === 'ios') {
+            if (RNNeteaseIm.getNetWorkStatus() === '1') {
+                return true;
+            }
+            return false;
+        } else {
             return NetInfo.isConnected.fetch().done();
         }
     }
+
     /** ******************************播放 ****************************************** **/
 
     /**
      * 播放录音
      * @returns {*}
      */
-    play(filepath){
+    play(filepath) {
         return RNNeteaseIm.play(filepath);
     }
+
     /**
      * 播放本地资源音乐
      * name：iOS：文件名字，Android：文件路径
      * type：音乐类型，如：'mp3'
      * @returns {*}
      */
-    playLocacl(name,type){
-        if(Platform.OS === 'ios'){
-            return RNNeteaseIm.playLocal(name,type);
+    playLocacl(name, type) {
+        if (Platform.OS === 'ios') {
+            return RNNeteaseIm.playLocal(name, type);
         }
-        return RNNeteaseIm.playLocal('assets:///'+name+'.'+type,type);
+        return RNNeteaseIm.playLocal('assets:///' + name + '.' + type, type);
     }
+
     /**
      * 停止播放录音
      * @returns {*}
      */
-    stopPlay(){
+    stopPlay() {
         return RNNeteaseIm.stopPlay();
     }
-    sortPinYin(o,key){
-        return PinYin.sortPinYin(o,key);
+
+    sortPinYin(o, key) {
+        return PinYin.sortPinYin(o, key);
     }
 
 
-    fetchNetInfo(){
+    /**
+     * 仅限Android
+     * @returns {*}
+     */
+    fetchNetInfo() {
         return RNNeteaseIm.fetchNetInfo();
     }
-    
+
     /**
+     * 仅限IOS
      * 设置webview UA
      * @returns {*}
      */
-    setupWebViewUserAgent(){
-        if(Platform.OS === 'ios'){
-            RNNeteaseIm.setupWebViewUserAgent();
-        }
+    setupWebViewUserAgent() {
+        RNNeteaseIm.setupWebViewUserAgent();
     }
+}
 module.exports = new Utils();
 
