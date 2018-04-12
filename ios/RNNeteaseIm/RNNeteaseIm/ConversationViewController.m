91cdf670c3e9876c404a21cbc004c8e65577d330
internet5 <172039711@qq.com>
2018-04-12 23:35:50 +0800

    移除好友限制
    移除好友限制

--- ios/RNNeteaseIm/RNNeteaseIm/ConversationViewController.m
+++ ios/RNNeteaseIm/RNNeteaseIm/ConversationViewController.m
@@ -379,27 +379,21 @@
 -(void)sendAudioMessage:(  NSString *)file duration:(  NSString *)duration{
     if (file) {
         NIMMessage *message = [NIMMessageMaker msgWithAudio:file andeSession:_session];
-        if ([self isFriendToSendMessage:message]) {
-             [[[NIMSDK sharedSDK] chatManager] sendMessage:message toSession:_session error:nil];
-        }
+        [[[NIMSDK sharedSDK] chatManager] sendMessage:message toSession:_session error:nil];
     }
 }
 //发送文字消息
 -(void)sendMessage:(NSString *)mess andApnsMembers:(NSArray *)members{
     NIMMessage *message = [NIMMessageMaker msgWithText:mess andApnsMembers:members andeSession:_session];
     //发送消息
-    if ([self isFriendToSendMessage:message]) {
-        [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
-    }
+    [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
 }
 //发送图片
 -(void)sendImageMessages:(  NSString *)path  displayName:(  NSString *)displayName{
     UIImage *img = [[UIImage alloc]initWithContentsOfFile:path];
     NIMMessage *message = [NIMMessageMaker msgWithImage:img andeSession:_session];
 //    NIMMessage *message = [NIMMessageMaker msgWithImagePath:path];
-    if ([self isFriendToSendMessage:message]) {
-        [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
-    }
+    [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
 }
 
 //发送视频
@@ -410,9 +404,7 @@
     //    }else{
     message = [NIMMessageMaker msgWithVideo:path andeSession:_session];
     //    }
-    if ([self isFriendToSendMessage:message]) {
-       [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
-    }
+   [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
     [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
     
 }
@@ -423,9 +415,7 @@
     NIMObject *obj = [NIMObject initNIMObject];
     obj.attachment =attachment;
     message = [NIMMessageMaker msgWithCustom:obj andeSession:_session];
-    if ([self isFriendToSendMessage:message]) {
-        [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
-    }
+    [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
 }
 
 //发送地理位置消息
@@ -433,9 +423,7 @@
     NIMLocationObject *locaObj = [[NIMLocationObject alloc]initWithLatitude:[latitude doubleValue] longitude:[longitude doubleValue] title:address];
     NIMKitLocationPoint *locationPoint = [[NIMKitLocationPoint alloc]initWithLocationObject:locaObj];
     NIMMessage *message = [NIMMessageMaker msgWithLocation:locationPoint andeSession:_session];
-    if ([self isFriendToSendMessage:message]) {
-        [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
-    }
+    [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
 }
 //发送自定义消息2
 -(void)sendCustomMessage:(NSInteger )custType data:(NSDictionary *)dataDict{
@@ -444,9 +432,7 @@
     obj.custType = custType;
     obj.dataDict = dataDict;
     message = [NIMMessageMaker msgWithCustomAttachment:obj andeSession:_session];
-    if ([self isFriendToSendMessage:message]) {
-        [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
-    }
+    [[NIMSDK sharedSDK].chatManager sendMessage:message toSession:_session error:nil];
 }
 
 //发送提醒消息
