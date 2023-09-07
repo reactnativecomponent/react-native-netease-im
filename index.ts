36a1e4d067ecc898ec99974d19674fedd334b17a
Ha Duc Manh <haducmanh10@gmail.com>
2023-09-07 16:56:48 +0700

    feat: add type event lister

--- index.ts
+++ index.ts
@@ -4,4 +4,21 @@ import NimTeam from "./src/Team/Team";
 import NimSystemMsg from "./src/SystemMsg/SystemMsg";
 import NimFriend from "./src/Friend/Friend";
 
+/**
+ *Event Listener
+ *observeRecentContact recent session
+ *observeOnlineStatus online status
+ *observeFriend contact/friend
+ *observeTeam group
+ *observeBlackList blacklist
+ *observeReceiveMessage Receive message
+ *observeReceiveSystemMsg system notification
+ *observeUnreadCountChange Number of unread messages
+ *observeMsgStatus sends message status changes
+ *observeAudioRecord recording status
+ *observeDeleteMessage Delete message after cancellation
+ *observeAttachmentProgress No reading changes
+ *observeOnKick was kicked offline
+ */
+
 export { NimSession, NimFriend, NimSystemMsg, NimTeam, NimUtils };
